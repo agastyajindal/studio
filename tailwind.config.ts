3376b97c54bf4b9fa722c7efbce4a1e5f5f9dbce
Agastya Jindal <agastyajindal@gmail.com>
2025-05-22 08:42:24 +0000

    the header buttons have a lag when we use them to their respective functionality, Optimize that and remove useless timed animations if there are none that make a major effect on said buttons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,7 +92,7 @@ export default {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in-up': 'fade-in-up 0.6s ease-out forwards',
+        'fade-in-up': 'fade-in-up 0.3s ease-out forwards', // Updated duration from 0.6s
   		}
   	}
   },
